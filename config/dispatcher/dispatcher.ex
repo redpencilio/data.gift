ad079a35aae87e68b806f3b6b7aee455c4b6394b
Erika Pauwels <erika.pauwels@gmail.com>
2019-09-23 14:54:15 +0200

    Add vocabulary and application profile for tasks

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -18,16 +18,24 @@ defmodule Dispatcher do
   # docker-compose stop; docker-compose rm; docker-compose up
   # after altering this file.
   #
-  get "/vocabularies/*path" do
-    Proxy.forward conn, path, "http://static-file/vocabularies/"
+  get "/vocabularies/:name/:version/*path" do
+    Proxy.forward conn, [], "http://static-file/vocabularies/" <> name <> "/" <> version <> "/index.ttl"
+  end
+
+  get "/vocabularies/:name/" do
+    Proxy.forward conn, [], "http://static-file/vocabularies/" <> name <> "/index.ttl"
+  end
+
+  get "/application-profiles/:name/:version/*path" do
+    Proxy.forward conn, [], "http://static-file/application-profiles/" <> name <> "/" <> version <> "/index.html"
+  end
+
+  get "/application-profiles/:name" do
+    Proxy.forward conn, [], "http://static-file/application-profiles/" <> name <> "/index.html"
   end
 
   get "/contexts/*path" do
     Proxy.forward conn, path, "http://static-file/contexts/"
-  end
-
-  get "/application-profiles/*path" do
-    Proxy.forward conn, path, "http://static-file/application-profiles/"
   end
 
   match _ do
